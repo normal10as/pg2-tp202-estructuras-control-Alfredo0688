69ca5f6becfe83ca290d405f1a5f42b4732650ef
Alfredo0688 <alfredonunez0688@gmail.com>
2018-05-21 19:02:05 -0300

    Arreglado el ej 2_05

--- Estructuras_Control_Flujo/2_05.vb
+++ Estructuras_Control_Flujo/2_05.vb
@@ -15,35 +15,35 @@
         Diciembre = 12
     End Enum
     Sub main()
-        Dim valor As meses
-        Console.Write("Ingrese un valor: ")
-        valor = Console.ReadLine
 
-        Select Case valor
-            Case 1
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 2
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 3
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 4
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 5
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 6
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 7
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 8
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 9
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 10
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 11
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
-            Case 12
-                Console.Write("El mes del año correspondiente es: " & valor.ToString)
+        Console.Write("Ingrese un valor: ")
+        mes = Console.ReadLine
+
+        Select Case mes
+            Case meses.Enero
+                Console.Write("El mes del año correspondiente es Enero")
+            Case meses.Febrero
+                Console.Write("El mes del año correspondiente es Febrero")
+            Case meses.Marzo
+                Console.Write("El mes del año correspondiente es Marzo")
+            Case meses.Abril
+                Console.Write("El mes del año correspondiente es Abril")
+            Case meses.Mayo
+                Console.Write("El mes del año correspondiente es Mayo")
+            Case meses.Junio
+                Console.Write("El mes del año correspondiente es Junio ")
+            Case meses.Julio
+                Console.Write("El mes del año correspondiente es Julio ")
+            Case meses.Agosto
+                Console.Write("El mes del año correspondiente es Agosto ")
+            Case meses.Septiembre
+                Console.Write("El mes del año correspondiente es Septiembre ")
+            Case meses.Octubre
+                Console.Write("El mes del año correspondiente es Octubre ")
+            Case meses.Noviembre
+                Console.Write("El mes del año correspondiente es Noviembre ")
+            Case meses.Diciembre
+                Console.Write("El mes del año correspondiente es Diciembre ")
         End Select
         Console.ReadKey()
     End Sub
